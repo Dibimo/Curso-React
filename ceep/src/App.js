a52deab7c8d2bf845372826132bf5a333aa2e6f9
Dibimo <diogobissolimoreno@gmail.com>
2021-03-20 14:57:49 -0300

    Formulário e lista de notas adicionados

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -4,7 +4,23 @@ const titulo = <h1><strong><em>Teste</em></strong></h1>
 
 function App() {
   return (
-    titulo
+    <>
+      <form>
+        <input type="text" placeholder='Título'></input>
+        <textarea placeholder='Escreva sua nota'></textarea>
+        <button>Criar nota</button>
+      </form>
+      <ul> 
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    </>
   );
 }
 
