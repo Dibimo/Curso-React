3aaacb5008454e1c12e248d861c2ba73b78629d5
Dibimo <diogobissolimoreno@gmail.com>
2021-03-22 11:23:52 -0300

    Estilos CSS aplicados nos componentes

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import  Formulario  from './components/Formulario/Formulario';
 import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas"
+import './App.css';
 
 class App extends Component {
   render(){
     return (
-      <>
+      <section className="conteudo">
         <Formulario/>
         <ListaDeNotas/>
-      </>
+      </section>
     );
   }
 }
--- ceep/src/components/Formulario/Formulario.jsx
+++ ceep/src/components/Formulario/Formulario.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-
+import "./estilo.css"
  class Formulario extends Component {
     render() {
         return (
-            <form>
-                <input type="text" placeholder='Título'></input>
-                <textarea placeholder='Escreva sua nota'></textarea>
-                <button>Criar nota</button>
+            <form className="form-cadastro ">
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro_input"
+                />
+                <textarea
+                    rows={15}
+                    placeholder="Escreva sua nota..."
+                    className="form-cadastro_input"
+                />
+                <button className="form-cadastro_input form-cadastro_submit">
+                    Criar Nota
+                </button>
             </form>
         );
     }
