41da14b6d755a6ab54989606493489620b199643
Dibimo <diogobissolimoreno@gmail.com>
2021-03-25 13:14:07 -0300

    Feito :D

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -5,15 +5,30 @@ import './assets/App.css';
 
 class App extends Component {
 
+  constructor(){
+    super();
+    this.state = {
+      notas: []
+    };
+  }
+
   criarNota(titulo,texto){
-    console.log(titulo + texto)
+    const novaNota = {
+      titulo,
+      texto
+    };
+    const novoArrayNotas = [...this.state.notas, novaNota]; //desconstroi o vetor antigo e adicionada uma nova nota
+    const novoEstado = {
+      notas:novoArrayNotas //recebe o vetor com a nova nota
+    }
+    this.setState(novoEstado);
   }
 
   render(){
     return (
       <section className="conteudo">
-        <Formulario criarNota={this.criarNota}/>
-        <ListaDeNotas/>
+        <Formulario criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
