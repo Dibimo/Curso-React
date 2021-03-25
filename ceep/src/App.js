f35b07010105b43771db38b9238aebd774917cab
Dibimo <diogobissolimoreno@gmail.com>
2021-03-25 10:57:45 -0300

    Quase adicionando notas

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -4,10 +4,15 @@ import ListaDeNotas from "./components/ListaDeNotas/index"
 import './assets/App.css';
 
 class App extends Component {
+
+  criarNota(titulo,texto){
+    console.log(titulo + texto)
+  }
+
   render(){
     return (
       <section className="conteudo">
-        <Formulario/>
+        <Formulario criarNota={this.criarNota}/>
         <ListaDeNotas/>
       </section>
     );
